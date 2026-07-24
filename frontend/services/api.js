5a0669a9a9b679bc612b9a202d375a572b3674d6
himanshujain79 <jainhimanshu0408@gmail.com>
2026-07-25 00:18:05 +0530

    Update API URL for production

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://notelet-backend.onrender.com/api",
 });
 export const getNotes = () => API.get("/notes");
 export const createNote = (note) => API.post("/notes", note);
